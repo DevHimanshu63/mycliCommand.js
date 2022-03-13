b2d9b5affd8bf42b2b161b08eb50a6e44267a5e6
DevHimanshu63 <hsc95067@gmail.com>
2022-03-13 22:32:12 +0530

    help command complete

--- main.js
+++ main.js
@@ -1,19 +1,20 @@
 // entryPoint start here !
 
 let helpFunc= require("./command/help")
-
 let inputarr=process.argv.slice(2);
 
 let command=inputarr[0];
+let path=imputarr[1];
 
 switch (command) {
         case "tree":
-        
+        console.log("hello tree"+path);
         break;
         case "organize":
-            
+            console.log("hello organize"+path);
         break;
         case "help":
+            // console.log(helpFunc.help);
         helpFunc.help();
         break;
         default:
